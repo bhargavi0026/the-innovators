379464b5719eac7c237f638acdca8debb4ed676e
email <nakkabhargavi.22@gmail.com>
2025-10-25 14:54:44 +0530

    add comment describing BrowserRouter setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,6 +18,9 @@ root.render(
 	</BrowserRouter>
 	// </React.StrictMode>
 );
+
+// ✅ Small change for Git practice
+console.log('App started successfully!');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
